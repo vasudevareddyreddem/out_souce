53db2fa3d0c02fceac516e66457982601088d2d3
vasudevareddyreddem <vasudevareddy549@gmail.com>
2018-06-07 21:23:17 +0530

    commit
    changes

--- index.php
+++ index.php
@@ -118,8 +118,65 @@ group with the aim of disseminating information for the betterment of mankind.</
                    
                 </div>
 				<br>
-				<div class="bg-primary text-white">
-					bdajdsajd
+				    
+				<div class="panel panel-primary">
+					  <div class="panel-heading clearfix">
+						<span class="pull-left"><h4 class="text-white">Latest Article</h4></span>
+						<a  href="#" class="pull-right"> <span style="color:#fff"> View all</span>&nbsp;&nbsp; </a>
+					  </div>
+					 
+				</div>
+				<div class="clearfix">&nbsp;</div>
+				<div class="article">
+					<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="article-head clearfix">
+					<span class="pull-left"><h4 class="text-white">Research Article</h4></span>
+
+					</div>
+					</div>
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="article-body clearfix">
+					<div class="row">
+					<div class="col-lg-2 col-md-3 col-sm-2 col-xs-4 p0">
+					<div class="article-view">
+					<div class="tabl">
+					<img src="https://juniperpublishers.com/admin-juniper/uploads/editorial-board/2118854779JFSCI_.jpg" align="left" alt="Article Image">
+					</div>
+					</div>
+					</div>
+					<div class="col-lg-10 col-md-9 col-sm-10 col-xs-8">
+					<div class="article-details">
+					<h4><a href="#" target="_blank" style="color:#0062C4;">An Exploration into the Acceptance of Male Rape Myths within the UK</a></h4>
+					<span class="name">Elizabeth Spruin* and Miss Laura Reilly</span>
+					<h6><a href="#" target="_blank" style="color:#F60;">Journal of Forensic Sciences &amp; Criminal Investigation </a></h6>
+						  <p class="art-date">DOI:<a href="http://dx.doi.org/10.19080/JFSCI.2018.09.555763" target="_blank">10.19080/JFSCI.2018.09.555763</a></p>
+					  </div>
+					</div>
+					</div>
+					<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="get-here">
+					<ul>
+					<li>
+					<a href="" target="_blank" class="btn btn-primary " style="font-size:12px;"> <i class="fa fa-unlock-alt"></i>&nbsp; PDF</a>
+					</li>
+					<li>
+					<a href="" target="_blank" class="btn btn-warning " style="font-size:12px;"> <i class="fa fa-code" aria-hidden="true"></i> HTML</a>
+					</li>
+					<li>
+					<a href="" target="_blank" class="btn btn-success " style="font-size:12px;">  <i class="fa fa-calendar" aria-hidden="true"></i> &nbsp; June 07, 2018</a>
+					</li>
+					<li>
+					
+					</li>
+					</ul>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div>
 				</div>
 				<div class="article">
 					<div class="row">
@@ -156,7 +213,210 @@ group with the aim of disseminating information for the betterment of mankind.</
 					<a href="" target="_blank" class="btn btn-primary " style="font-size:12px;"> <i class="fa fa-unlock-alt"></i>&nbsp; PDF</a>
 					</li>
 					<li>
-					<a href="" target="_blank" class="btn btn-warning " style="font-size:12px;"> <i class="fa fa-unlock-alt"></i>&nbsp; FullText</a>
+					<a href="" target="_blank" class="btn btn-warning " style="font-size:12px;"> <i class="fa fa-code" aria-hidden="true"></i> HTML</a>
+					</li>
+					<li>
+					<a href="" target="_blank" class="btn btn-success " style="font-size:12px;">  <i class="fa fa-calendar" aria-hidden="true"></i> &nbsp; June 07, 2018</a>
+					</li>
+					<li>
+					
+					</li>
+					</ul>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div>
+				</div>
+				<div class="article">
+					<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="article-head clearfix">
+					<span class="pull-left"><h4 class="text-white">Research Article</h4></span>
+
+					</div>
+					</div>
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="article-body clearfix">
+					<div class="row">
+					<div class="col-lg-2 col-md-3 col-sm-2 col-xs-4 p0">
+					<div class="article-view">
+					<div class="tabl">
+					<img src="https://juniperpublishers.com/admin-juniper/uploads/editorial-board/2118854779JFSCI_.jpg" align="left" alt="Article Image">
+					</div>
+					</div>
+					</div>
+					<div class="col-lg-10 col-md-9 col-sm-10 col-xs-8">
+					<div class="article-details">
+					<h4><a href="#" target="_blank" style="color:#0062C4;">An Exploration into the Acceptance of Male Rape Myths within the UK</a></h4>
+					<span class="name">Elizabeth Spruin* and Miss Laura Reilly</span>
+					<h6><a href="#" target="_blank" style="color:#F60;">Journal of Forensic Sciences &amp; Criminal Investigation </a></h6>
+						  <p class="art-date">DOI:<a href="http://dx.doi.org/10.19080/JFSCI.2018.09.555763" target="_blank">10.19080/JFSCI.2018.09.555763</a></p>
+					  </div>
+					</div>
+					</div>
+					<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="get-here">
+					<ul>
+					<li>
+					<a href="" target="_blank" class="btn btn-primary " style="font-size:12px;"> <i class="fa fa-unlock-alt"></i>&nbsp; PDF</a>
+					</li>
+					<li>
+					<a href="" target="_blank" class="btn btn-warning " style="font-size:12px;"> <i class="fa fa-code" aria-hidden="true"></i> HTML</a>
+					</li>
+					<li>
+					<a href="" target="_blank" class="btn btn-success " style="font-size:12px;">  <i class="fa fa-calendar" aria-hidden="true"></i> &nbsp; June 07, 2018</a>
+					</li>
+					<li>
+					
+					</li>
+					</ul>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div>
+				</div>
+				<div class="article">
+					<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="article-head clearfix">
+					<span class="pull-left"><h4 class="text-white">Research Article</h4></span>
+
+					</div>
+					</div>
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="article-body clearfix">
+					<div class="row">
+					<div class="col-lg-2 col-md-3 col-sm-2 col-xs-4 p0">
+					<div class="article-view">
+					<div class="tabl">
+					<img src="https://juniperpublishers.com/admin-juniper/uploads/editorial-board/2118854779JFSCI_.jpg" align="left" alt="Article Image">
+					</div>
+					</div>
+					</div>
+					<div class="col-lg-10 col-md-9 col-sm-10 col-xs-8">
+					<div class="article-details">
+					<h4><a href="#" target="_blank" style="color:#0062C4;">An Exploration into the Acceptance of Male Rape Myths within the UK</a></h4>
+					<span class="name">Elizabeth Spruin* and Miss Laura Reilly</span>
+					<h6><a href="#" target="_blank" style="color:#F60;">Journal of Forensic Sciences &amp; Criminal Investigation </a></h6>
+						  <p class="art-date">DOI:<a href="http://dx.doi.org/10.19080/JFSCI.2018.09.555763" target="_blank">10.19080/JFSCI.2018.09.555763</a></p>
+					  </div>
+					</div>
+					</div>
+					<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="get-here">
+					<ul>
+					<li>
+					<a href="" target="_blank" class="btn btn-primary " style="font-size:12px;"> <i class="fa fa-unlock-alt"></i>&nbsp; PDF</a>
+					</li>
+					<li>
+					<a href="" target="_blank" class="btn btn-warning " style="font-size:12px;"> <i class="fa fa-code" aria-hidden="true"></i> HTML</a>
+					</li>
+					<li>
+					<a href="" target="_blank" class="btn btn-success " style="font-size:12px;">  <i class="fa fa-calendar" aria-hidden="true"></i> &nbsp; June 07, 2018</a>
+					</li>
+					<li>
+					
+					</li>
+					</ul>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div>
+				</div>
+				<div class="article">
+					<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="article-head clearfix">
+					<span class="pull-left"><h4 class="text-white">Research Article</h4></span>
+
+					</div>
+					</div>
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="article-body clearfix">
+					<div class="row">
+					<div class="col-lg-2 col-md-3 col-sm-2 col-xs-4 p0">
+					<div class="article-view">
+					<div class="tabl">
+					<img src="https://juniperpublishers.com/admin-juniper/uploads/editorial-board/2118854779JFSCI_.jpg" align="left" alt="Article Image">
+					</div>
+					</div>
+					</div>
+					<div class="col-lg-10 col-md-9 col-sm-10 col-xs-8">
+					<div class="article-details">
+					<h4><a href="#" target="_blank" style="color:#0062C4;">An Exploration into the Acceptance of Male Rape Myths within the UK</a></h4>
+					<span class="name">Elizabeth Spruin* and Miss Laura Reilly</span>
+					<h6><a href="#" target="_blank" style="color:#F60;">Journal of Forensic Sciences &amp; Criminal Investigation </a></h6>
+						  <p class="art-date">DOI:<a href="http://dx.doi.org/10.19080/JFSCI.2018.09.555763" target="_blank">10.19080/JFSCI.2018.09.555763</a></p>
+					  </div>
+					</div>
+					</div>
+					<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="get-here">
+					<ul>
+					<li>
+					<a href="" target="_blank" class="btn btn-primary " style="font-size:12px;"> <i class="fa fa-unlock-alt"></i>&nbsp; PDF</a>
+					</li>
+					<li>
+					<a href="" target="_blank" class="btn btn-warning " style="font-size:12px;"> <i class="fa fa-code" aria-hidden="true"></i> HTML</a>
+					</li>
+					<li>
+					<a href="" target="_blank" class="btn btn-success " style="font-size:12px;">  <i class="fa fa-calendar" aria-hidden="true"></i> &nbsp; June 07, 2018</a>
+					</li>
+					<li>
+					
+					</li>
+					</ul>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div>
+				</div><div class="article">
+					<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="article-head clearfix">
+					<span class="pull-left"><h4 class="text-white">Research Article</h4></span>
+
+					</div>
+					</div>
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="article-body clearfix">
+					<div class="row">
+					<div class="col-lg-2 col-md-3 col-sm-2 col-xs-4 p0">
+					<div class="article-view">
+					<div class="tabl">
+					<img src="https://juniperpublishers.com/admin-juniper/uploads/editorial-board/2118854779JFSCI_.jpg" align="left" alt="Article Image">
+					</div>
+					</div>
+					</div>
+					<div class="col-lg-10 col-md-9 col-sm-10 col-xs-8">
+					<div class="article-details">
+					<h4><a href="#" target="_blank" style="color:#0062C4;">An Exploration into the Acceptance of Male Rape Myths within the UK</a></h4>
+					<span class="name">Elizabeth Spruin* and Miss Laura Reilly</span>
+					<h6><a href="#" target="_blank" style="color:#F60;">Journal of Forensic Sciences &amp; Criminal Investigation </a></h6>
+						  <p class="art-date">DOI:<a href="http://dx.doi.org/10.19080/JFSCI.2018.09.555763" target="_blank">10.19080/JFSCI.2018.09.555763</a></p>
+					  </div>
+					</div>
+					</div>
+					<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="get-here">
+					<ul>
+					<li>
+					<a href="" target="_blank" class="btn btn-primary " style="font-size:12px;"> <i class="fa fa-unlock-alt"></i>&nbsp; PDF</a>
+					</li>
+					<li>
+					<a href="" target="_blank" class="btn btn-warning " style="font-size:12px;"> <i class="fa fa-code" aria-hidden="true"></i> HTML</a>
 					</li>
 					<li>
 					<a href="" target="_blank" class="btn btn-success " style="font-size:12px;">  <i class="fa fa-calendar" aria-hidden="true"></i> &nbsp; June 07, 2018</a>
